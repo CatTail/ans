564520522be1cd0bc7811382200bf3837d9a7147
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2014-08-15 15:17:41 +0800

    fix: rule not update after changes

--- apps/proxy/index.js
+++ apps/proxy/index.js
@@ -6,10 +6,10 @@ var minimatch = require('minimatch'),
 var app;
 app = exports = module.exports = require('express')();
 
-Rule.getRuleList(function(err, rules) {
-    if (err) { throw err; }
+app.use(function (req, res, next) {
+    Rule.getRuleList(function(err, rules) {
+        if (err) { throw err; }
 
-    app.use(function (req, res, next) {
         var match = rules.some(function(rule) {
             if (minimatch(req.header('host'), rule.host)) {
                 proxy.web(req, res, { target: rule.address }, next);
@@ -20,13 +20,13 @@ Rule.getRuleList(function(err, rules) {
             next();
         }
     });
+});
 
-    app.use(function(req, res, next) {
-        res.status(404).send('Not Found');
-    });
+app.use(function(req, res, next) {
+    res.status(404).send('Not Found');
+});
 
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.send(err.message || err.stack || err);
-    });
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message || err.stack || err);
 });
